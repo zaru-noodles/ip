3e718bc5c1812cb03298801b2bd164bb8cdf0598
Aidan Tay <silver14211@gmail.com>
2026-08-12 14:29:38 +0800

    Change chatbot name from Duke to Zaru

---
+++ src/main/java/Zaru.java
@@ -0,0 +1,11 @@
+/** Entry point for the Zaru chatbot application. */
+public class Zaru {
+    public static void main(String[] args) {
+        String banner = " _____                   \n"
+                + "|__  /__ _ _ __ _   _    \n"
+                + "  / // _` | '__| | | |   \n"
+                + " / /| (_| | |  | |_| |   \n"
+                + "/____\\__,_|_|   \\__,_|   \n";
+        System.out.println(banner);
+    }
+}
