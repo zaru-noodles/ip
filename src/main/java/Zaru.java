1572c9644640713ba7a332f79adefc65bea99cf2
Aidan Tay <silver14211@gmail.com>
2026-08-12 15:10:01 +0800

    Add echo functionality

--- src/main/java/Zaru.java
+++ src/main/java/Zaru.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 /** Entry point for the Zaru chatbot application. */
 public class Zaru {
     private static final String LINE_SEPARATOR = "____________________________________________________________";
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
         System.out.println(LINE_SEPARATOR);
 
         String banner = " _____                   \n"
@@ -11,7 +15,29 @@ public class Zaru {
                 + " / /| (_| | |  | |_| |   \n"
                 + "/____\\__,_|_|   \\__,_|   \n";
         printMessage(banner + "Hello! I'm Zaru.\nWhat can I do for you?");
-        printMessage("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String message = inputMessage(scanner);
+            processMessage(message);
+            if (message.equals("bye")) {
+                break;
+            }
+        }
+    }
+
+    private static String inputMessage(Scanner scanner) {
+        String msg = scanner.nextLine();
+        System.out.println(LINE_SEPARATOR);
+        return msg;
+    }
+
+    private static void processMessage(String message) {
+        message = message.trim();
+        if (message.equals("bye")) {
+            printMessage("Bye. Hope to see you again soon!");
+        } else {
+            printMessage(message);
+        }
     }
 
     private static void printMessage(String message) {
