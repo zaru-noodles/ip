b975840eb14a4bb14c4a06ab5f558482651c20c1
Aidan Tay <silver14211@gmail.com>
2026-08-12 14:43:33 +0800

    Add initial greeting and exit

--- src/main/java/Zaru.java
+++ src/main/java/Zaru.java
@@ -1,11 +1,20 @@
 /** Entry point for the Zaru chatbot application. */
 public class Zaru {
+    private static final String LINE_SEPARATOR = "____________________________________________________________";
+
     public static void main(String[] args) {
+        System.out.println(LINE_SEPARATOR);
+
         String banner = " _____                   \n"
                 + "|__  /__ _ _ __ _   _    \n"
                 + "  / // _` | '__| | | |   \n"
                 + " / /| (_| | |  | |_| |   \n"
                 + "/____\\__,_|_|   \\__,_|   \n";
-        System.out.println(banner);
+        printMessage(banner + "Hello! I'm Zaru.\nWhat can I do for you?");
+        printMessage("Bye. Hope to see you again soon!");
+    }
+
+    private static void printMessage(String message) {
+        System.out.println(message + "\n" + LINE_SEPARATOR);
     }
 }
