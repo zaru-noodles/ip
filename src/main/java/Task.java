18b9607da16f4fa801e46ecd1482be44bd56925f
Aidan Tay <silver14211@gmail.com>
2026-08-13 15:46:55 +0800

    Add task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    private String title;
+    private boolean completed;
+
+    public Task(String title) {
+        this.title = title;
+        this.completed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", completed ? "[x]" : "[ ]", title);
+    }
+}
