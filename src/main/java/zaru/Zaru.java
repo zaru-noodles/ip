1e986d935225e8ab9ec8b8eb9db535fe735feb77
Aidan Tay <silver14211@gmail.com>
2026-08-19 15:33:13 +0800

    Update main to support new Command class

--- src/main/java/zaru/Zaru.java
+++ src/main/java/zaru/Zaru.java
@@ -2,6 +2,8 @@ package zaru;
 
 import java.nio.file.Path;
 
+import zaru.command.ByeCommand;
+import zaru.command.Command;
 import zaru.exception.ZaruException;
 import zaru.parser.Parser;
 import zaru.storage.Storage;
@@ -29,67 +31,15 @@ public class Zaru {
         while (true) {
             String message = UI.retrieveMessage();
             try {
-                processMessage(message);
+                Command cmd = Parser.parseMessage(message);
+                cmd.execute(tasks);
+
+                if (cmd instanceof ByeCommand) {
+                    break;
+                }
             } catch (ZaruException e) {
                 UI.sendError(e.getMessage());
             }
-            if (message.equalsIgnoreCase("bye")) {
-                break;
-            }
-        }
-    }
-
-    /** Processes a command by adding a task, listing tasks, or ending the session. */
-    private static void processMessage(String message) throws ZaruException {
-        Parser.ParsedMessage data = Parser.parseMessage(message);
-
-        switch (data.command) {
-        case "bye" -> UI.sendMessage("Bye. Hope to see you again soon!");
-        case "list" -> {
-            if (tasks.size() == 0) {
-                UI.sendMessage("You have no tasks!");
-            } else {
-                UI.sendMessage("Here are your tasks:\n%s".formatted(tasks));
-            }
-        }
-        case "mark" -> {
-            int i = parseNumber(data.arg);
-            validateTaskNumber(i);
-            tasks.complete(i);
-            UI.sendMessage("Meow! I've marked that task as done!\n%s".formatted(tasks.getTaskString(i)));
-        }
-        case "unmark" -> {
-            int i = parseNumber(data.arg);
-            validateTaskNumber(i);
-            tasks.uncomplete(i);
-            UI.sendMessage("I've unmarked that task!\n%s".formatted(tasks.getTaskString(i)));
-        }
-        case "delete" -> {
-            int i = parseNumber(data.arg);
-            validateTaskNumber(i);
-            String tmp = tasks.getTaskString(i);
-            tasks.delete(i);
-            UI.sendMessage("I've deleted that task!\n%s".formatted(tmp));
-        }
-        case "todo" -> {
-            validateNonEmpty(data.arg, "The description of a todo cannot be empty.");
-            tasks.add(new ToDo(data.arg));
-            UI.printAddTaskMessage(tasks);
-        }
-        case "deadline" -> {
-            validateNonEmpty(data.arg, "The description of a deadline cannot be empty.");
-            validateNonEmpty(data.keyArgs.get("by"), "Please provide a deadline date using /by.");
-            tasks.add(new Deadline(data.arg, data.keyArgs.get("by")));
-            UI.printAddTaskMessage(tasks);
-        }
-        case "event" -> {
-            validateNonEmpty(data.arg, "The description of an event cannot be empty.");
-            validateNonEmpty(data.keyArgs.get("from"), "Please provide an event start time using /from.");
-            validateNonEmpty(data.keyArgs.get("to"), "Please provide an event end time using /to.");
-            tasks.add(new Event(data.arg, data.keyArgs.get("from"), data.keyArgs.get("to")));
-            UI.printAddTaskMessage(tasks);
-        }
-        default -> throw new ZaruException("Sorry, I don't know what that means ;w;");
         }
     }
 }
